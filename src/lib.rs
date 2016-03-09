f81422fb82572d164e5a7d4dd63e9dd3f2bb954d
Lucio Franco <lucio.franco@du.edu>
2016-03-09 14:00:00 -0700

    cleaned up public BitMap interface and added save method

--- src/lib.rs
+++ src/lib.rs
@@ -18,31 +18,32 @@ pub use image::{Image, Pixel};
 
 /// `BitMap` represents a bitmap image with a `Header` and `Body`
 pub struct BitMap {
-    header: Header,
     body: Body,
 }
 
 // TODO: have bitmap support any buffer
 impl BitMap {
     pub fn new() -> Self {
-        BitMap {
-            header: Header::new(),
-            body: Body::default(),
-        }
+        BitMap { body: Body::default() }
     }
 
-    pub fn load<B: Write + Read + Seek>(mut buf: &mut B) -> Result<Self, Error> {
+    pub fn load<B: Read + Seek>(mut buf: &mut B) -> Result<BitMap, Error> {
         let mut header = Header::new();
         try!(header.load(&mut buf));
 
-        Ok(BitMap {
-            header: header,
-            body: Body::default(),
-        })
+        let mut body = Body::new(header);
+        try!(body.load(&mut buf));
+
+        Ok(BitMap { body: body })
+    }
+
+    pub fn save<B: Write + Seek>(&mut self, mut buf: &mut B) -> Result<(), Error> {
+        self.body.save(&mut buf)
     }
 }
 
 /// `Error` are errors that can happen during runtime
+#[derive(Debug)]
 pub enum Error {
     ReadHeader,
     Byteorder(byteorder::Error),
