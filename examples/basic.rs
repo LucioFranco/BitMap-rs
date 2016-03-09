c8e1c4615be8412c915f21b84cc60c4a699a09aa
Lucio Franco <lucio.franco@du.edu>
2016-03-09 14:00:00 -0700

    implemented basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,12 @@
 extern crate bitmap;
 
 use bitmap::BitMap;
+use std::fs::File;
 
 fn main() {
-    let bm = BitMap::new();
+    let mut buf = File::open("images/train.bmp").unwrap();
+    let mut bm = BitMap::load(&mut buf).unwrap();
 
+    let mut new_buf = File::create("target/train example.bmp").unwrap();
+    bm.save(&mut new_buf);
 }
