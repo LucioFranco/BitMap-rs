af601bb46b6bc9a183a526fad62bf3660403e9a8
Lucio Franco <lucio.franco@du.edu>
2016-03-12 12:39:45 -0700

    added example 32bit save

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,8 +4,11 @@ use bitmap::BitMap;
 use std::fs::File;
 
 fn main() {
-    let mut buf = File::open("images/train.bmp").unwrap();
+    let mut buf = File::open("images/mountain.bmp").unwrap();
     let mut bm = BitMap::load(&mut buf).unwrap();
+
+    let mut new_buf = File::create("target/mountain example.bmp").unwrap();
+    bm.save(&mut buf);
 
     let mut buf = File::open("images/train.bmp").unwrap();
     let mut bm = BitMap::load(&mut buf).unwrap();
