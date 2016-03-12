ad8e1666a310f7bec36f59fd0162139b34110378
Lucio Franco <lucio.franco@du.edu>
2016-03-12 12:36:09 -0700

    added 32 bit example

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,6 +7,9 @@ fn main() {
     let mut buf = File::open("images/train.bmp").unwrap();
     let mut bm = BitMap::load(&mut buf).unwrap();
 
+    let mut buf = File::open("images/train.bmp").unwrap();
+    let mut bm = BitMap::load(&mut buf).unwrap();
+
     let mut new_buf = File::create("target/train example.bmp").unwrap();
     bm.save(&mut new_buf);
 }
